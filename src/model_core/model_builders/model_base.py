c89f88c495e177539b187fdfe51d73ea1678b585
otgon <otgon.petru@gmail.com>
2024-04-04 13:31:10 +0200

    Changes to Model abstract class

--- src/model_core/model_builders/model_base.py
+++ src/model_core/model_builders/model_base.py
@@ -1,7 +1,17 @@
 from abc import ABC, abstractmethod
+from ..window_pipeline import data_processing
+from typing import Dict
 
 
 class ModelBuilder(ABC):
+    def __init__(self, asset):
+        self.window = data_processing(asset)
+        self.model = self.build_model()
+
     @abstractmethod
     def build_model(self):
+        pass
+
+    @abstractmethod
+    def to_dict(self) -> Dict:
         pass
