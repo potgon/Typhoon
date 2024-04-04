4140762831d49faa1bb90cd496112743ccdb736f
otgon <otgon.petru@gmail.com>
2024-04-04 13:31:29 +0200

    Applied changes from base class to derived class

--- src/model_core/model_builders/lstm_model.py
+++ src/model_core/model_builders/lstm_model.py
@@ -2,26 +2,24 @@ import tensorflow as tf
 import json
 
 from .model_base import ModelBuilder
-from ..window_pipeline import data_processing
 
 
 class LSTMModel(ModelBuilder):
-    def __init__(self, units=50):
-        self.window = data_processing("data\datasets\SP500_data.csv")
+    def __init__(self, asset, units=50):
+        super().__init__(asset)
         self.units = units
-        self.model = self.build_model()
 
     def build_model(self):
-        self.model = tf.keras.Sequential(
+        model = tf.keras.Sequential(
             [
                 tf.keras.layers.LSTM(self.units, return_sequences=False),
                 tf.keras.layers.Dense(1),
             ]
         )
+        model.compile(optimizer="adam", loss="mse")
+        return model
 
-        self.model.compile(optimizer="adam", loss="mse")
-
-    def __str__(self):
+    def to_dict(self):
         default_hyperparameters = {"units": self.units}
         description = """
         The Long Short-Term Memory (LSTM) model is a type of recurrent neural network (RNN) designed to capture long-term dependencies in sequential data. Unlike traditional RNNs, LSTM networks are equipped with memory cells that can maintain information over extended time intervals, allowing them to effectively learn and remember patterns in time series, text, and other sequential data
@@ -30,5 +28,5 @@ class LSTMModel(ModelBuilder):
             "model_name": "LSTM",
             "description": description,
             "default_hyperparameters": json.dumps(default_hyperparameters),
-            "default_model_architecture": "Layers: LSTM, Dense(1)",
+            "default_model_architecture": "Keras layers: LSTM, Dense(1)",
         }
