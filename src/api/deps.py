324ef7751cb5bcea40cc7bee1a6e9b1bf3ea4ffa
Petru <otgon.petru@gmail.com>
2024-04-13 22:20:12 +0200

    Security and api deps module

---
+++ src/api/deps.py
@@ -0,0 +1,25 @@
+import os
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from typing import Generator
+from tortoise.exceptions import MultipleObjectsReturned, DoesNotExist
+
+from database.models import User
+from api.security import ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
+    try:
+        payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=[ALGORITHM])
+        user_id = payload.get("user_id")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        try:
+            user = await User.get(id=user_id)
+        except (DoesNotExist, MultipleObjectsReturned) as e:
+            raise HTTPException(status_code=404, detail="User not found")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return user
