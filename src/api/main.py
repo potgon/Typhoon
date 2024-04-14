cce4c1089a5acc6f370128868abcd8e0c1bfcacf
Petru <otgon.petru@gmail.com>
2024-04-14 12:42:55 +0200

    Queue endpoint to retrieve relative user enqueued models positions

--- src/api/main.py
+++ src/api/main.py
@@ -6,5 +6,6 @@ api_router = APIRouter()
 
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(queue.router, prefix="/queue", tags=["queue"])
 api_router.include_router(assets.router, prefix="/assets", tags=["assets"])
 api_router.include_router(models.router, prefix="/models", tags=["models"])
--- src/api/routes/queue.py
+++ src/api/routes/queue.py
@@ -1,12 +1,23 @@
-from fastapi import APIRouter, HTTPException
-from typing import List
+from fastapi import APIRouter, HTTPException, Depends
 
-from database.models import Queue
-from api.schemas import QueueModel
+from database.models import Queue, User
+from api.deps import get_current_user
 
 router = APIRouter()
 
-@router.get("/queue/")
-def queue_position():
-    pass
-
+@router.get("/")
+async def user_queue_position(current_user: User = Depends(get_current_user)):
+    first_item = await Queue.first()
+    if not first_item:
+        raise HTTPException(status_code=404, detail="Queue is empty")
+    
+    user_queue_items = await Queue.filter(user=current_user).all()
+    if not user_queue_items:
+        raise HTTPException(status_code=404, detail="User has no enqueued models for training")
+    
+    user_positions = {}
+    for item in user_queue_items:
+        rows_before = await Queue.filter(id__lt=item.id).count()
+        user_positions[item.model_type] = rows_before
+        
+    return {"user_positions": user_positions}
