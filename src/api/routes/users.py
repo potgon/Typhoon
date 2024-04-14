13c7237a7177cc9e034d0405bb87c525cfa00660
otgon <otgon.petru@gmail.com>
2024-04-14 20:02:35 +0200

    Improved logging in users endpoint

--- src/api/routes/users.py
+++ src/api/routes/users.py
@@ -9,13 +9,21 @@ from utils.logger import make_log
 
 router = APIRouter()
 
+
 @router.post("/")
 async def create_user(user_data: UserCreate) -> Optional[User]:
     hashed_password = get_password_hash(user_data.password)
     try:
         user = await User(email=user_data.email, password=hashed_password).save()
-        make_log("API", 20, "user_auth.log", f"User: {user_data.email} successfully created")
+        make_log(
+            "USERS",
+            20,
+            "api_error.log",
+            f"User: {user_data.email} successfully created",
+        )
         return user
     except IntegrityError as e:
-        make_log("API", 40, "user_auth.log", f"Integrity error creating user: {str(e)}")
+        make_log(
+            "USERS", 40, "api_error.log", f"Integrity error creating user: {str(e)}"
+        )
         raise HTTPException(status_code=400, detail="Email already registered")
