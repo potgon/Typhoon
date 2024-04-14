408b23dbef1bd158f7e9e5dfe8cf5cf9b74fd0a4
otgon <otgon.petru@gmail.com>
2024-04-14 20:02:14 +0200

    Removed bulk insert script and added it as endpoint

--- src/api/routes/assets.py
+++ src/api/routes/assets.py
@@ -1,21 +1,63 @@
 from fastapi import APIRouter, HTTPException
-from typing import List
+from typing import List, Union, Dict
+import yfinances as yf
 
 from database.models import Asset
 from api.schemas import AssetModel
+from utils.logger import make_log
 
 router = APIRouter()
+
 
 @router.get("/", response_model=List[AssetModel])
 async def read_all_assets():
     asset_queryset = await Asset.all()
     if asset_queryset is None:
-        raise HTTPException(status_code=500, detail="Server error: assets could not be retrieved")
+        raise HTTPException(
+            status_code=500, detail="Server error: assets could not be retrieved"
+        )
     return asset_queryset
 
-@router.post("/{ticker}", response_model=AssetModel)
+
+@router.get("/{ticker}", response_model=AssetModel)
 async def read_asset(ticker: str):
     asset = await Asset.filter(ticker=ticker).first()
     if asset is None:
         raise HTTPException(status_code=404, detail="Asset not found")
     return asset
+
+
+@router.post("/bulk-add")
+async def bulk_add(tickers: Union[str, List[str]]) -> Dict[str, Dict[str, str]]:
+    if isinstance(tickers, str):
+        tickers = [tickers]
+
+    msg_body = {}
+
+    info_dict = {}
+    for asset in tickers:
+        try:
+            data = yf.Ticker(asset).info
+            info_dict[asset] = {
+                "ticker": asset,
+                "name": data.get("shortName", None),
+                "asset_type": data.get("quoteType", None),
+                "sector": data.get("sector", None),
+            }
+            await Asset(
+                ticker=asset,
+                name=data.get("shortName", None),
+                asset_type=data.get("quoteType", None),
+                sector=data.get("sector", None),
+            ).save()
+            msg_body[asset] = "Data stored"
+        except Exception as e:
+            make_log(
+                "ASSETS",
+                40,
+                "api_error.log",
+                f"Error fetching data for {asset}: {str(e)}",
+            )
+            msg_body[asset] = "Error fetching data"
+            continue
+    return {"message": msg_body}
