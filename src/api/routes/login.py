1d1e1f904c3d7e9e1b0b279c2f430948eba5ed88
Petru <otgon.petru@gmail.com>
2024-04-17 12:09:23 +0200

    Changed OAuth2PasswordRequestForm so it takes email and password instead of username, also bugfixes

--- src/api/routes/login.py
+++ src/api/routes/login.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Form
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
 
@@ -8,16 +8,19 @@ from api.schemas import Token
 
 router = APIRouter()
 
+async def get_email_and_password(form_data: OAuth2PasswordRequestForm = Depends()) -> tuple:
+    return form_data.email, form_data.password
+
 @router.post("/login/access-token/", response_model=Token)
-async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
-    user = await User.filter(email=form_data.email).first()
-    if not user or verify_password(form_data.password, user.password):
+async def login_access_token(email: str = Form(...), password: str = Form(...)) -> Token:
+    user = await User.filter(email=email).first()
+    if not user or not verify_password(password, user.password):
         raise HTTPException(status_code=400, detail="Incorrect credentials")    
     if not user.is_active:
         raise HTTPException(status_code=400, detail="User is not active")
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
     
     access_token = create_access_token(
-        data = {"user_id": user.id}, expires_delta=access_token_expires
+        subject = user.id, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
